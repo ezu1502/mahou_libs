bf9635c2e6a0c13c266c88c01c8bd8ecf6a376f0
Ezu1502 <ezu150208@gmail.com>
2026-07-17 15:12:27 -0300

    point functions added

--- mahou_libs/__init__.py
+++ mahou_libs/__init__.py
@@ -1,3 +1,3 @@
-from .colors import painted_string, COLORS
-from . import mahou_math
-from . import time_functions
+# from .colors import painted_string, COLORS
+# from . import mahou_math
+# from . import time_functions
--- mahou_libs/time_functions.py
+++ mahou_libs/time_functions.py
@@ -5,7 +5,9 @@ from typing import Callable, ParamSpec, TypeVar
 
 from .colors import painted_string
 
-log = logging.getLogger(painted_string("mahou_libs.time_functions", "#FDFF77"))
+from.bocca import BoccaFiglia
+
+log = BoccaFiglia("mahou_libs.time_functions", "#FDFF77")
 
 P = ParamSpec("P")
 R = TypeVar("R")
@@ -20,10 +22,34 @@ def log_delta_time(function: Callable[P, R]) -> Callable[P, R]:
         finally:
             delta = time.perf_counter() - before
             meantime = painted_string(f"{delta:.6f}", "#FFFB00")
-            log.debug(
+            log.timetracking(
                 "%s executed, action took %s seconds",
                 function.__qualname__,
                 meantime,
                 )
 
     return wrapper
+
+def print_delta_time(function: Callable[P, R]) -> Callable[P, R]:
+    @wraps(function)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
+        before = time.perf_counter()
+        try:
+            return function(*args, **kwargs)
+        finally:
+            delta = time.perf_counter() - before
+            meantime = painted_string(f"{delta:.6f}", "#FFFB00")
+            print(
+                "%s executed, action took %s seconds",
+                function.__qualname__,
+                meantime,
+                )
+    return wrapper
+
+
+def first_point():
+    point1 = time.perf_counter()
+
+def second_point(point):
+    point2 = time.perf_counter()
+    log.timetracking(f"{point2 - point}")
